f40cc99efea93e1279d7dd527ab96c38b797c68e
Adam Buxton <buxton.adam@gmail.com>
2022-10-28 16:22:08 +0100

    remove var

--- variables.tf
+++ variables.tf
@@ -7,10 +7,10 @@ variable "oauth_name" {
   type        = string
   description = "(Required) Name of OAuth VCS connection you created"
 }
-variable "organization" {
-  type        = string
-  description = "(Required) Organization name"
-}
+# variable "organization" {
+#   type        = string
+#   description = "(Required) Organization name"
+# }
 variable "terraform_server" {
   type        = string
   description = "(Optional) defaults to app.terraform.io, but otherwise the FQDN of your TFE server"
