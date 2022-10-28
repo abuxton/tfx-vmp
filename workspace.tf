76af784a88db741a12c5e70d4f002944ac3b2aae
Adam Buxton <buxton.adam@gmail.com>
2022-10-28 16:25:47 +0100

    .

--- workspace.tf
+++ workspace.tf
@@ -1,6 +1,6 @@
 resource "tfe_workspace" "component-env" {
   name           = "component-env"
-  organization   = local.organization
+  organization   = local.organization["0"]
   execution_mode = "local"
   tag_names      = ["component", "vmp", "environment"]
 }
