3ed47a350e146bb18f954ac28927b8a47d030edf
Adam Buxton <buxton.adam@gmail.com>
2022-10-31 16:54:56 +0000

    data.ref update

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ data "environment_variables" "ATLAS" {
 }
 
 locals {
-  list_organization_workspace = split("/", data.environment_variables.ATLAS.ATLAS_WORKSPACE_SLUG)
+  list_organization_workspace = split("/", data.environment_variables.ATLAS.environment.items.ATLAS_WORKSPACE_SLUG)
   organization                = local.list_organization_workspace["0"]
   workspace                   = local.list_organization_workspace["1"]
 }
