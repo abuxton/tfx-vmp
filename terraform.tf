5b66dbbd2f4580e9bb4ac225d4dcd25100c078be
Adam Buxton <buxton.adam@gmail.com>
2022-12-22 10:58:53 +0000

    add provider

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,26 @@
 terraform {
+  # cloud {
+  #   organization = "abc-vmp"
+
+  #   workspaces {
+  #     name = "vmp_workspace"
+  #   }
+  # }
+
   required_providers {
     tfe = {
       source  = "hashicorp/tfe"
       version = "0.38.0"
     }
+    environment = {
+      source  = "EppO/environment"
+      version = "1.3.3"
+    }
   }
+}
+
+provider "environment" {
+  # Configuration options
 }
 
 provider "tfe" {
