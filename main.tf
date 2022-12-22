7f68a84a4af5176d7847b972618c309314f77e9e
Adam Buxton <buxton.adam@gmail.com>
2022-12-22 11:04:00 +0000

    utilise provider

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-
+data "environment_variables" "atlas_slug" {
+  filter = "^ATLAS_WORKSPACE_SLUG"
+}
 locals {
-  list_organization_workspace = split("/", var.ATLAS_WORKSPACE_SLUG)
+  list_organization_workspace = split("/", data.environment_variables.atlas_slug)
   organization                = local.list_organization_workspace["0"]
   workspace                   = local.list_organization_workspace["1"]
 }
