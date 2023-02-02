3796854cbac9bf3393938ca294d318838e0202dd
Adam Buxton <buxton.adam@gmail.com>
2023-02-02 08:31:36 +0000

    update outputs

--- output.tf
+++ output.tf
@@ -2,9 +2,9 @@ output "test-agent-token" {
   value     = nonsensitive(tfe_agent_token.test-agent-token.token)
   sensitive = false
 }
-output "environment_variables_all" {
-  value = data.environment_variables.all.items
-}
+# output "environment_variables_all" {
+#   value = data.environment_variables.all.items
+# }
 output "atlas_workspace_slug" {
   value = data.environment_variables.atlas_slug
 }
