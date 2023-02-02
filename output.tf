4523d186349608b5fbcfa0050aa4036e39fc6f50
Adam Buxton <buxton.adam@gmail.com>
2023-02-02 08:22:49 +0000

    .

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "test-agent-token" {
-  value     = nonsensitive(tfe_agent_token.test-agent-token.token)
-  sensitive = false
-}
+# output "test-agent-token" {
+#   value     = nonsensitive(tfe_agent_token.test-agent-token.token)
+#   sensitive = false
+# }
 output "environment_variables_all" {
   value = data.environment_variables.all.items
 }
--- team.tf
+++ team.tf
@@ -1,19 +1,19 @@
-resource "tfe_team" "component-env" {
-  name         = "component-env"
-  organization = local.organization
-  visibility   = "organization"
-  # organization_access {
-  #   manage_workspaces = true
-  #   manage_policies   = true
-  # }
-  provider = tfe.organization
-}
-resource "tfe_team_access" "component-env" {
-  access       = "read"
-  team_id      = tfe_team.component-env.id
-  workspace_id = tfe_workspace.component-env.id
-}
-
-# resource "tfe_team_token" "component-env" {
-#   team_id = tfe_team.component-env.id
+# resource "tfe_team" "component-env" {
+#   name         = "component-env"
+#   organization = local.organization
+#   visibility   = "organization"
+#   # organization_access {
+#   #   manage_workspaces = true
+#   #   manage_policies   = true
+#   # }
+#   provider = tfe.organization
 # }
+# resource "tfe_team_access" "component-env" {
+#   access       = "read"
+#   team_id      = tfe_team.component-env.id
+#   workspace_id = tfe_workspace.component-env.id
+# }
+
+# # resource "tfe_team_token" "component-env" {
+# #   team_id = tfe_team.component-env.id
+# # }
