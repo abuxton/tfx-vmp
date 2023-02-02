e47fe34004b5ea4bb4761a1bd8720e7d64110e2a
Adam Buxton <buxton.adam@gmail.com>
2023-02-02 08:21:44 +0000

    .

--- cloud_agent.tf
+++ cloud_agent.tf
@@ -1,10 +1,10 @@
-resource "tfe_agent_pool" "test-agent-pool" {
-  name         = "my-agent-pool-name"
-  organization = local.organization
-  provider     = tfe.organization
-}
-resource "tfe_agent_token" "test-agent-token" {
-  agent_pool_id = tfe_agent_pool.test-agent-pool.id
-  description   = "my-agent-token-name"
-  provider      = tfe.organization
-}
+# resource "tfe_agent_pool" "test-agent-pool" {
+#   name         = "my-agent-pool-name"
+#   organization = local.organization
+#   provider     = tfe.organization
+# }
+# resource "tfe_agent_token" "test-agent-token" {
+#   agent_pool_id = tfe_agent_pool.test-agent-pool.id
+#   description   = "my-agent-token-name"
+#   provider      = tfe.organization
+# }
