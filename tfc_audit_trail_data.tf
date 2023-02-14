047af961b7f10fc53dca73bf18678ecb1bf3e710
Adam Buxton <buxton.adam@gmail.com>
2023-02-14 15:22:46 +0000

    org not required

--- tfc_audit_trail_data.tf
+++ tfc_audit_trail_data.tf
@@ -1,7 +1,7 @@
 
 data "http" "tfc_audit_trail" {
   #https://developer.hashicorp.com/terraform/cloud-docs/api-docs/audit-trails
-  url    = "https://app.terraform.io/api/v2/${local.organization}/audit-trail?page[number]=1"
+  url    = "https://app.terraform.io/api/v2/organization/audit-trail?page[number]=1"
   method = "GET"
   request_headers = {
     Authorization = "Bearer ${var.TFx_org_token}"
