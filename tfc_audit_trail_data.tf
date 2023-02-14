9e23b7687e0fa861d7e395c93580edae3242d159
Adam Buxton <buxton.adam@gmail.com>
2023-02-14 15:46:40 +0000

    update ref var to local

--- tfc_audit_trail_data.tf
+++ tfc_audit_trail_data.tf
@@ -1,7 +1,7 @@
 
 data "http" "tfc_audit_trail" {
   #https://developer.hashicorp.com/terraform/cloud-docs/api-docs/audit-trails
-  url    = "https://app.terraform.io/api/v2/organization/audit-trail?since=${var.audit_timestamp}"
+  url    = "https://app.terraform.io/api/v2/organization/audit-trail?since=${local.audit_timestamp}"
   method = "GET"
   request_headers = {
     Authorization = "Bearer ${var.TFx_org_token}"
