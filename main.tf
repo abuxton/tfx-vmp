21be6aa5120790c16844b0086925465242736149
Adam Buxton <buxton.adam@gmail.com>
2023-02-14 15:53:45 +0000

    timey whimey changes

--- main.tf
+++ main.tf
@@ -9,5 +9,5 @@ locals {
   list_organization_workspace = split("/", data.environment_variables.atlas_slug.items.ATLAS_WORKSPACE_SLUG)
   organization                = local.list_organization_workspace["0"]
   workspace                   = local.list_organization_workspace["1"]
-  audit_timestamp             = var.audit_timestamp != "" ? var.audit_timestamp : timestamp()
+  audit_timestamp             = var.audit_timestamp != "" ? var.audit_timestamp : timeadd(timestamp(), "-10s")
 }
